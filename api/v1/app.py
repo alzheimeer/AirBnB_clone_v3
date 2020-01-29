1c12b444b03a7034ef83deb86dd231692d5f8abb
charlyhackr <930@holbertonschool.com>
2020-01-29 15:49:01 -0500

    fix code for  tas 4 and  6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,38 @@
-#!/usr/bin/pyhton3
-"""create start api!"""
+#!/usr/bin/python3
+""" Flask app """
 
-from flask import Flask, jsonify
 from models import storage
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
-from os import getenv
+from os import environ
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """A method to handle @app.teardown_appcontext that calls storage.close"""
+def teardown_request(exception=None):
+    """
+    Method to close the connection when the execution finish
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def error404(error):
-    """create a handler for 404 errors that returns a JSON-formatted 404
-    status code response"""
-    error = {"error": "Not found"}
-    return jsonify(error), 404
+def not_found(error):
+    """
+    Method to manage the url's that doesn't exists
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST") or "0.0.0.0"
-    port = getenv("HBNB_API_PORT") or 5000
-    app.run(host=host, port=port, threaded=True, debug=True)
+    """
+    Main function
+    """
+    my_host = environ.get("HBNB_API_HOST")
+    my_port = environ.get("HBNB_API_PORT")
+    app.run(host=my_host, port=my_port, threaded=True)
