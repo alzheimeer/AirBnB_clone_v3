38232bca690e3609250d0757bac1e510f4d03484
alzheimeer <fogniebla@hotmail.com>
2020-01-30 22:14:49 -0500

    fix cities list ignored

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,14 +6,14 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 
 
-@app_views.route('/states', methods=["GET"], strict_slashes=False)
-def statesAll():
-    """Retrieves all states with a list of objects"""
-    ll = []
-    s = storage.all('State').values()
-    for v in s:
-        ll.append(v.to_dict())
-    return jsonify(ll)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def states_list():
+    """Retrieves the list of all State objects"""
+    states_list = []
+    states_objs = storage.all('State').values()
+    for element in states_objs:
+        states_list.append(element.to_dict())
+    return jsonify(states_list)
 
 
 @app_views.route('/states/<state_id>', methods=["GET"], strict_slashes=False)
